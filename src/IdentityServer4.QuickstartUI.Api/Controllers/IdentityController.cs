bcf6684253a2e9a089e1e7c0c089cba3c574fbb4
CodeHedgehog <codehedgehog@outlook.com>
2018-07-19 18:04:08 -0700

    Add API client (console app) project

---
+++ src/IdentityServer4.QuickstartUI.Api/Controllers/IdentityController.cs
@@ -0,0 +1,21 @@
+﻿
+namespace IdentityServer4.QuickstartUI.Api.Controllers
+{
+	using Microsoft.AspNetCore.Authorization;
+	using Microsoft.AspNetCore.Mvc;
+	using System.Linq;
+
+
+	[Route("identity")]
+	[ApiController]
+	[Authorize]
+	public class IdentityController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+		}
+	}
+
+}
---
+++ src/IdentityServer4.QuickstartUI.ApiClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IdentityServer4.QuickstartUI.ApiClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
